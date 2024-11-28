00b37063f0daf0adc809098731c79f7d4377cf33
mark <zhoutianwen.999@gmail.com>
2024-11-28 22:09:05 +1300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,9 @@
-import {
-  Admin,
-  Resource,
-  ListGuesser,
-  EditGuesser,
-  ShowGuesser,
-} from "react-admin";
-import { Layout } from "./Layout";
-import { dataProvider } from "./dataProvider";
-import { authProvider } from "./authProvider";
+// in src/App.tsx
+import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from 'react-admin';
+import { dataProvider } from './dataProvider';
 
 export const App = () => (
-  <Admin
-    layout={Layout}
-    dataProvider={dataProvider}
-    authProvider={authProvider}
-  >
-    <Resource
-      name="posts"
-      list={ListGuesser}
-      edit={EditGuesser}
-      show={ShowGuesser}
-    />
-    <Resource
-      name="comments"
-      list={ListGuesser}
-      edit={EditGuesser}
-      show={ShowGuesser}
-    />
+  <Admin dataProvider={dataProvider}>
++   <Resource name="users" list={ListGuesser} />
   </Admin>
 );
--- src/dataProvider.ts
+++ src/dataProvider.ts
@@ -1,8 +1,13 @@
-import fakeRestDataProvider from "ra-data-fakerest";
+// import fakeRestDataProvider from "ra-data-fakerest";
+import jsonServerProvider from 'ra-data-json-server';
 import data from "./data.json";
 
-export const dataProvider = fakeRestDataProvider(
-  data,
-  process.env.NODE_ENV !== "test",
-  300,
+// export const dataProvider = fakeRestDataProvider(
+//   data,
+//   process.env.NODE_ENV !== "test",
+//   300,
+// );
+
+export const dataProvider = jsonServerProvider(
+  import.meta.env.VITE_JSON_SERVER_URL
 );
