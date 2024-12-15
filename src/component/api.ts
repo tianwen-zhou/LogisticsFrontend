bfe8dbeecbc1eb72279ac30bd16493513732134c
mark <zhoutianwen.999@gmail.com>
2024-12-16 10:06:28 +1300

    update task map display with data from backend

--- src/component/api.ts
+++ src/component/api.ts
@@ -1,8 +1,7 @@
 export const fetchTaskData = async (taskId: string) => {
-    try {
-      console.log(`/api/route/getTaskRoute/${taskId}`);
-      const response = await fetch(`http://localhost:5137/api/route/getTaskRoute/${taskId}`);
-      console.log('@response',response);
+  const apiUrl = import.meta.env.VITE_JSON_SERVER_URL;  
+  try {
+      const response = await fetch(`${apiUrl}/route/getTaskRoute/${taskId}`);
       if (!response.ok) {
         throw new Error(`Failed to fetch task data: ${response.status}`);
       }
